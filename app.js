04700d901bc228b1e7341550158496dfc42969f9
sorrycc <sorrycc@gmail.com>
2015-05-27 11:31:46 +0800

    feat: redirect /arale-tip to /arale-tip/latest

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var http = require('http');
 var join = require('path').join;
+var fs = require('fs');
 
 global.CONFIG = {
   wwwroot: join(__dirname, '../spmjs.io/data')
@@ -12,6 +13,13 @@ var app = express();
 app.set('port', 3002);
 
 app.get('/:name/:version/*', require('./docs'));
+app.get('/:name', function(req, res, next) {
+  if (fs.existsSync(join(CONFIG.wwwroot, 'docs', req.params.name))) {
+    res.redirect('/' + req.params.name + '/latest');
+  } else {
+    next();
+  }
+});
 app.get('/*', express.static(join(CONFIG.wwwroot, 'docs')));
 
 http.createServer(app).listen(app.get('port'), function() {
